9bb2cfe4b3659cd0d824c96b0b63ee86411828fa
Wei Chen <smile.v.chen@gmail.com>
2023-05-21 08:51:57 +0900

    Change messages of errors by using the beginning of lower-case letter

--- errors.go
+++ errors.go
@@ -4,13 +4,13 @@ import "errors"
 
 var (
 	// ErrEmptyPath is returned when the path argument is empty
-	ErrEmptyPath = errors.New("Path argument is empty")
+	ErrEmptyPath = errors.New("path argument is empty")
 	// ErrReadFile is returned when failed to read a file
-	ErrReadFile = errors.New("Failed to read a file")
+	ErrReadFile = errors.New("failed to read a file")
 	// ErrHTTPRequest is returned when failed to make a request by http client
-	ErrHTTPRequest = errors.New("Failed to create a http request")
+	ErrHTTPRequest = errors.New("failed to create a http request")
 	// ErrHTTPResponse is returned when failed to receive a response by http client
-	ErrHTTPResponse = errors.New("Failed to receive a http response")
+	ErrHTTPResponse = errors.New("failed to receive a http response")
 	// ErrHTTPRespBody is returned when failed to read a http response body
-	ErrHTTPRespBody = errors.New("Failed to read a http response body")
+	ErrHTTPRespBody = errors.New("failed to read a http response body")
 )
