e4d9996bf38640c3643a0f23c99a6595f4040239
Wei Chen <smile.v.chen@gmail.com>
2023-06-11 15:47:15 +0900

    Implement customized data

---
+++ customized_data.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+type CustomizedData map[string]string
+
+func (ctd CustomizedData) Apply(raw json.RawMessage) string {
+	result := string(raw)
+	if len(ctd) == 0 {
+		return result
+	}
+
+	for key, value := range ctd {
+		wrappedKey := wrap(key)
+		result = strings.Replace(result, wrappedKey, value, 1)
+	}
+
+	return result
+}
+
+func wrap(k string) string {
+	return fmt.Sprintf("#{%s}", k)
+}
