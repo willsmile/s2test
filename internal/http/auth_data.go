be577b6865ae7b83239e5eeca9ea6e6ee20d3e44
Wei Chen <smile.v.chen@gmail.com>
2023-06-21 13:48:00 +0900

    Create type AuthData and type AuthDataset

---
+++ internal/http/auth_data.go
@@ -0,0 +1,35 @@
+package http
+
+const (
+	AuthTypeCookie = "Cookie"
+	AuthTypeOAuth2 = "OAuth 2.0"
+)
+
+// AuthData is data for authentication
+type AuthData map[string]string
+
+// AuthDataset is a dataset to store AuthData
+type AuthDataset map[string]AuthData
+
+func (s AuthDataset) Select(method string) AuthData {
+	return s[method]
+}
+
+func (data AuthData) isCookie() bool {
+	return data["type"] == AuthTypeCookie
+}
+
+func (data AuthData) isOAuth2() bool {
+	return data["type"] == AuthTypeOAuth2
+}
+
+func (data AuthData) NewAuthInfo() AuthInfo {
+	var info AuthInfo
+
+	if data.isCookie() {
+		info = cookie{cookieName: data["name"], cookieValue: data["value"]}
+	} else if data.isOAuth2() {
+		info = token{tokenPrefix: data["prefix"], tokenValue: data["value"]}
+	}
+	return info
+}
